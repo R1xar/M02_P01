eaa00fa2bf3610e54e33f3eeff758ed58ecbcc36
R1xar <ricardo.lopez.macedo@gmail.com>
2021-09-10 17:21:05 -0500

    Se agrega el archivo rap y el auto generado cpp

---
+++ M02_P01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? salida;
+   ?? ddrb;
+   ?? pina;
+   ?? ddra;
+   ?? portb;
+
+   ddrb =255;
+   ddra =0;
+   while (1)
+   {
+      raptor_prompt_variable_zzyz ="Emulacion de accion de leer el puerto";
+      cout << raptor_prompt_variable_zzyz << endl;
+      cin >> pina;
+      portb =pina;
+      salida =false;
+      if (salida==true)) break;
+   }
+
+   return 0;
+}
